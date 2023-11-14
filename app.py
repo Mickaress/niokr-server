de262ba6632a5b98a70e69cfbc92373c90a2d9a7
Mickaress <kaishevmichael@gmail.com>
2023-11-14 18:25:42 +0800

    add get vacancies route

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello world"
+def connect_db():
+    return sqlite3.connect('database.db')
+
+
+# Получение списка вакансий
+@app.route("/api/vacancies")
+def get_vacancies():
+    with connect_db() as connection:
+        cursor = connection.cursor()
+
+        # Общее количество записей для пагинации на фронте
+        cursor.execute("SELECT COUNT(*) FROM vacancies WHERE is_accept = 1")
+        amount = cursor.fetchone()[0]
+
+        # Получение списка вакансий на выбранной странице
+        page = int(request.args.get('page', 1))
+        items_per_page = 3
+        offset = (page - 1) * items_per_page
+        limit = items_per_page
+
+        cursor.execute("SELECT * FROM vacancies WHERE is_accept = 1 LIMIT ? OFFSET ?", (limit, offset))
+
+        columns = [column[0] for column in cursor.description]
+        vacancies = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    return jsonify({'vacancies': vacancies, 'amount': amount})
 
 
 if __name__ == '__main__':
